ea55a0d15948a02da88fffff7a8b9273252fa8cd
QuangPham <ptquang0941@outlook.com>
2019-06-16 14:20:35 +0700

    Update proc_menu_top

---
+++ Document/store procedure/proc_menu_top.sql
@@ -0,0 +1,40 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `MENU_TOPs`(
+	IN `ACCOUNTID` INT,
+	IN `GROUPMENU` CHAR ( 2 ) CHARSET UTF8,
+	IN `MENUSTATUS` CHAR ( 2 ) CHARSET UTF8,
+	IN MENUID CHAR ( 100 ) 
+	)
+    NO SQL
+BEGIN#load menu top join 2 table menu_navication and sub_menu_navigation
+	IF
+		( MENUSTATUS = '-1' && ACCOUNTID = - 1 && GROUPMENU = '-1' ) THEN
+		SELECT
+			sub_menu_name,
+			sub_menu_link,
+			sub_menu_parameter,
+			icon,
+			sub_menu_tooltip,
+			menu_id 
+		FROM
+			sub_menu_navigation 
+		WHERE
+			menu_id IN ( MENUID );
+		ELSE SELECT
+			alias,
+			menu_link,
+			menu_parameter,
+			menu_tooltip,
+			icon,
+			menu_id 
+		FROM
+			menu_navication 
+		WHERE
+			acc_id = ACCOUNTID -- check account of customer
+			
+			AND menu_status = MENUSTATUS -- status 0 enable 1 disable
+			
+			AND group_menu = GROUPMENU;-- group menu 0 menu top 1 menu bottom
+		
+	END IF;
+
+END
