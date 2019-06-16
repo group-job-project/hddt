b9a559939c531dfbf9dfc5d887709b4fd41feaa1
QuangPham <ptquang0941@outlook.com>
2019-06-16 18:16:44 +0700

    Update sql PROC menu_top

--- Document/store procedure/proc_menu_top.sql
+++ Document/store procedure/proc_menu_top.sql
@@ -1,40 +1,11 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `MENU_TOP`(
-	IN `ACCOUNTID` INT,
-	IN `GROUPMENU` CHAR ( 2 ) CHARSET UTF8,
-	IN `MENUSTATUS` CHAR ( 2 ) CHARSET UTF8,
-	IN MENUID INT 
-	)
-    NO SQL
-BEGIN #load menu top join 2 table menu_navication and sub_menu_navigation
-	IF
-		( MENUSTATUS = '-1' && ACCOUNTID = - 1 && GROUPMENU = '-1' ) THEN
-		SELECT
-			sub_menu_name,
-			sub_menu_link,
-			sub_menu_parameter,
-			icon,
-			sub_menu_tooltip,
-			menu_id 
-		FROM
-			sub_menu_navigation 
-		WHERE
-			menu_id = MENUID;
-		ELSE SELECT
-			alias,
-			menu_link,
-			menu_parameter,
-			menu_tooltip,
-			icon,
-			menu_id 
-		FROM
-			menu_navication 
-		WHERE
-			acc_id = ACCOUNTID -- check account of customer
-			
-			AND menu_status = MENUSTATUS -- status 0 enable 1 disable
-			
-			AND group_menu = GROUPMENU;-- group menu 0 menu top 1 menu bottom
-		
-	END IF;
-
+CREATE DEFINER=`root`@`localhost` PROCEDURE `MENU_TOP`()
+BEGIN
+	SELECT DISTINCT MENU.menu_id, MENU.alias, MENU.menu_link, MENU.menu_parameter, MENU.menu_tooltip,
+		MENU.icon,
+        IFNULL(SUB_MENU.sub_menu_name, "") AS sub_menu_name, IFNULL(SUB_MENU.sub_menu_link, "") AS sub_menu_link, IFNULL(SUB_MENU.sub_menu_parameter, "") AS sub_menu_parameter, IFNULL(SUB_MENU.icon, "") AS icon,
+		IFNULL(SUB_MENU.sub_menu_tooltip, "") AS sub_menu_tooltip
+	FROM menu_navication MENU
+		LEFT JOIN sub_menu_navigation SUB_MENU ON SUB_MENU.menu_id = MENU.menu_id
+	WHERE MENU.group_menu = 0
+	ORDER BY MENU.menu_id ASC, MENU.menu_sort ASC, SUB_MENU.sub_menu_sort ;
 END
