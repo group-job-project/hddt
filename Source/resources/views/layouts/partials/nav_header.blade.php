4fdbd43c467e19b28249dd2f358e0a5b3e530d9a
QuangPham <ptquang0941@outlook.com>
2019-06-16 14:24:37 +0700

    Update code menu top

--- Source/resources/views/layouts/partials/nav_header.blade.php
+++ Source/resources/views/layouts/partials/nav_header.blade.php
@@ -1,6 +1,3 @@
-<?php
-use App\Http\BusinessLogic\NavigationMenu;
-?>
 <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
 	<a class="navbar-brand mr-lg-0" href="#"><b>A.T&T</b> </a>
 	<button class="navbar-toggler p-0 border-0" type="button"
